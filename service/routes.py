e5541264d6326c4c91625b5b9d58bab46ed10b37
Rafal Kviatkovski <rafkakv@gmail.com>
2024-10-08 07:11:44 -0400

    m

--- service/routes.py
+++ service/routes.py
@@ -105,7 +105,6 @@ def list_products():
     results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
     return results, status.HTTP_200_OK
-
 def test_get_product_list(self):
     """It should Get a list of Products"""
     self._create_products(5)
@@ -122,22 +121,6 @@ def list_products():
     products = []
     name = request.args.get("name")
     if name:
-        app.logger.info("Find by name: %s", name)
-        products = Product.find_by_name(name)
-    else:
-        app.logger.info("Find all")
-        products = Product.all()
-    results = [product.serialize() for product in products]
-    app.logger.info("[%s] Products returned", len(results))
-    return results, status.HTTP_200_OK
-
-def test_query_by_name(self):
-    """It should Query Products by name"""
-    products = self._create_products(5)
-    test_name = products[0].name
-    name_count = len([product for product in products if product.name == test_name])
-    response = self.client.get(
-        BASE_URL, query_string=f"name={quote_plus(test_name)}"
     )
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     data = response.get_json()
@@ -160,23 +143,6 @@ def list_products():
     elif category:
         app.logger.info("Find by category: %s", category)
         # create enum from string
-        category_value = getattr(Category, category.upper())
-        products = Product.find_by_category(category_value)
-    else:
-        app.logger.info("Find all")
-        products = Product.all()
-    results = [product.serialize() for product in products]
-    app.logger.info("[%s] Products returned", len(results))
-    return results, status.HTTP_200_OK
-
-def test_query_by_category(self):
-    """It should Query Products by category"""
-    products = self._create_products(10)
-    category = products[0].category
-    found = [product for product in products if product.category == category]
-    found_count = len(found)
-    logging.debug("Found Products [%d] %s", found_count, found)
-
     # test for available
     response = self.client.get(BASE_URL, query_string=f"category={category.name}")
     self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -204,24 +170,7 @@ def list_products():
         category_value = getattr(Category, category.upper())
         products = Product.find_by_category(category_value)
     elif available:
-        app.logger.info("Find by available: %s", available)
-        # create bool from string
-        available_value = available.lower() in ["true", "yes", "1"]
-        products = Product.find_by_availability(available_value)
-    else:
-        app.logger.info("Find all")
-        products = Product.all()
-    results = [product.serialize() for product in products]
-    app.logger.info("[%s] Products returned", len(results))
-    return results, status.HTTP_200_OK
-
-def test_query_by_availability(self):
-    """It should Query Products by availability"""
-    products = self._create_products(10)
-    available_products = [product for product in products if product.available is True]
-    available_count = len(available_products)        
-    # test for available
-    response = self.client.get(
+        app.logger.info("Find by avail    response = self.client.get(
         BASE_URL, query_string="available=true"
     )
     self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -279,22 +228,6 @@ def update_products(product_id):
     product.update()
     return product.serialize(), status.HTTP_200_OK
 
-
-def test_update_product(self):
-    """It should Update an existing Product"""
-    # create a product to update
-    test_product = ProductFactory()
-    response = self.client.post(BASE_URL, json=test_product.serialize())
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    # update the product
-    new_product = response.get_json()
-    new_product["description"] = "unknown"
-    response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    updated_product = response.get_json()
-    self.assertEqual(updated_product["description"], "unknown")
-
 ######################################################################
 # D E L E T E   A   P R O D U C T
 ######################################################################
@@ -309,17 +242,3 @@ def delete_products(product_id):
     if product:
         product.delete()
     return "", status.HTTP_204_NO_CONTENT
-
-def test_delete_product(self):
-    """It should Delete a Product"""
-    products = self._create_products(5)
-    product_count = self.get_product_count()
-    test_product = products[0]
-    response = self.client.delete(f"{BASE_URL}/{test_product.id}")
-    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    self.assertEqual(len(response.data), 0)
-    # make sure they are deleted
-    response = self.client.get(f"{BASE_URL}/{test_product.id}")
-    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    new_count = self.get_product_count()
-    self.assertEqual(new_count, product_count - 1)
